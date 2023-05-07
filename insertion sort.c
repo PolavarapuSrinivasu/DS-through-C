0fe80ea6c418320cacce4975994336b6a09a061a
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2023-05-07 18:02:45 +0530

    Add files via upload

---
+++ insertion sort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int insertion_sort(int *a,int n)
+{
+	int i,j;
+	for(i=1;i<n;i++)
+	{
+		int key=a[i];
+		for(j=i-1;;j--)
+		{
+			if(a[j]<key || j==-1)
+			{
+				a[j+1]=key;
+				break;
+			}
+			else
+			{
+				a[j+1]=a[j];
+			}
+		}
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	insertion_sort(a,n);
+	for(i=0;i<n;i++)
+	{
+		printf("%d  ",a[i]);
+	}
+}
